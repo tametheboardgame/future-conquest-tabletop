9f94ccf84092facfe446704e572dbfb776a3d005
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2026-08-21 12:09:07 +0100

    R5-WP1.2: load prototype forces into board

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { CENTRAL_FRONT_BOARD } from './tabletop/board';
+import { CENTRAL_FRONT_PROTOTYPE_FORCE } from './tabletop/pieces';
 import { TabletopBoard } from './tabletop/TabletopBoard';
 
 export default function App() {
-  return <TabletopBoard board={CENTRAL_FRONT_BOARD} />;
+  return <TabletopBoard board={CENTRAL_FRONT_BOARD} force={CENTRAL_FRONT_PROTOTYPE_FORCE} />;
 }
